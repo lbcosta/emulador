58a1c1b192b889096821acaf4e8c4a6d93494501
Leonardo Costa <leocosta@edu.unifor.br>
2019-02-26 12:31:33 -0300

    Regex dos 4 comandos funcionando

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from parser import Parser
+
+parser = Parser()
+
+input = "inc E"
+
+print(parser.parse(input))
---
+++ src/parser.py
@@ -0,0 +1,14 @@
+import re 
+class Parser:
+    def parse(self, input_string):
+        reg = "[A-Z]" #Registradores = Qualquer letra
+        num = "\d+" #Numeros inteiros = Qualquer numero
+        mem = "0x\d+" #Espaço de Memoria = 0x(Qualquer numero)
+        expressions = [
+            f"^(mov)\s+({reg}),\s+({num})$", #mov
+            f"^(add)\s+({reg}|{mem}),\s+({reg}|{num})$", #add
+            f"^(inc)\s+({reg}|{mem})$", #inc
+            f"^(imul)\s+({reg}|{mem}),\s+({reg}|{mem}|{num}),\s+({reg}|{mem}|{num})$" #imul
+        ]
+        tests = [True if re.match(expression, input_string) else False for expression in expressions]
+        return any(tests)
