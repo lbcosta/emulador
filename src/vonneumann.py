64a3c6bbd4f5256448355e95092f6ee1d17eb6ea
Leonardo Costa <leocosta@edu.unifor.br>
2019-03-23 21:46:27 -0300

    Ajustando argumentos para iniciar app no prompt

--- src/vonneumann.py
+++ src/vonneumann.py
@@ -3,12 +3,11 @@ from helpers.PrintFormat import color_format
 import argparse
 
 argparser = argparse.ArgumentParser(description='Emulador de uma máquina de Von Neumann')
-argparser.add_argument('-f', '--file')
-argparser.add_argument('-a', '--arch')
+argparser.add_argument('file', help="Arquivo com código em assemly que deve ser executado")
+argparser.add_argument('arch', help="Arquitetura a ser emulada (8, 16, 32, 64)")
 args = argparser.parse_args()
 
 if args.arch and args.file:
     emulator = Emulator(args.arch)
     emulator.run(args.file)
-else:
-    raise ValueError(color_format("Informe o arquivo assembly a ser lido e uma arquiterura", "RED"))
+    
